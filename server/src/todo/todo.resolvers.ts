5010e24500dfabc249ea59fdce756b72f8abce7f
Chris Lee <sehlceris@protonmail.com>
2018-09-05 01:11:27 -0700

    refactor todo resolver to use base api

--- server/src/todo/todo.resolvers.ts
+++ server/src/todo/todo.resolvers.ts
@@ -1,104 +1,71 @@
 import {
-  HttpException,
-  HttpStatus,
   UseGuards,
 } from '@nestjs/common';
-import { isArray, map } from 'lodash';
-import { ToBooleanPipe } from '../shared/pipes/to-boolean.pipe';
-import { TodoLevel } from './models/todo-level.enum';
-import { Todo } from './models/todo.model';
-import { TodoParams } from './models/view-models/todo-params.model';
-import { TodoVm } from './models/view-models/todo-vm.model';
-import { TodoService } from './todo.service';
+import {ToBooleanPipe} from '../shared/pipes/to-boolean.pipe';
+import {TodoLevel} from './models/todo-level.enum';
+import {Todo} from './models/todo.model';
+import {TodoParams} from './models/view-models/todo-params.model';
+import {TodoVm} from './models/view-models/todo-vm.model';
+import {TodoService} from './todo.service';
 import {UserRole} from '../user/models/user-role.enum';
 import {Roles} from '../shared/decorators/roles.decorator';
-import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
+import {Args, Mutation, Query, Resolver, Subscription} from '@nestjs/graphql';
 import {PubSub} from 'graphql-subscriptions';
 import {GraphQLJwtAuthGuard} from '../shared/guards/graphql/graphql-jwt-auth-guard.service';
 import {GraphQLRolesGuard} from '../shared/guards/graphql/graphql-roles-guard.service';
+import {TodoApiBase} from './todo.api.base';
 
 const pubSub = new PubSub();
 
 @Resolver('Todo')
-export class TodoResolvers {
-  constructor(private readonly todoService: TodoService) {}
+export class TodoResolvers extends TodoApiBase {
+  constructor(
+    protected readonly todoService: TodoService,
+  ) {
+    super(todoService);
+  }
 
   @Query()
   @Roles(UserRole.Admin, UserRole.User)
   @UseGuards(GraphQLJwtAuthGuard, GraphQLRolesGuard)
   async getTodos(
     @Args('level') level?: TodoLevel,
-    @Args('isCompleted', new ToBooleanPipe()) isCompleted?: boolean
+    @Args('isCompleted', new ToBooleanPipe()) isCompleted?: boolean,
   ): Promise<TodoVm[]> {
-    let filter = {};
-
-    if (level) {
-      filter['level'] = { $in: isArray(level) ? [...level] : [level] };
-    }
-
-    if (typeof(isCompleted) === 'boolean') {
-      if (filter['level']) {
-        filter = { $and: [{ level: filter['level'] }, { isCompleted }] };
-      } else {
-        filter['isCompleted'] = isCompleted;
-      }
-    }
-
-    const todos = await this.todoService.findAll(filter);
-    return this.todoService.map<TodoVm[]>(
-      map(todos, (todo) => todo.toJSON())
-    );
+    return super.getTodos(level, isCompleted);
   }
 
   @Mutation('createTodo')
   @Roles(UserRole.Admin, UserRole.User)
-  // @UseGuards(<any>AuthGuard('jwt'), RolesGuard)
+  @UseGuards(GraphQLJwtAuthGuard, GraphQLRolesGuard)
   async createTodo(
-    @Args() args: TodoParams
+    @Args() args: TodoParams,
   ): Promise<TodoVm> {
-    const newTodo = await this.todoService.createTodo(args);
-    const mappedTodo = this.todoService.map<TodoVm>(newTodo);
-    pubSub.publish('todoCreated', { todoCreated: mappedTodo });
-    return mappedTodo;
+    const todoPromise = super.createTodo(args);
+    todoPromise
+      .then((mappedTodo) => {
+        // TODO: this publish must also somehow happen upon creation from the REST endpoints
+        pubSub.publish('todoCreated', {todoCreated: mappedTodo});
+      });
+    return todoPromise;
   }
 
   @Mutation('updateTodo')
   @Roles(UserRole.Admin, UserRole.User)
-  // @UseGuards(<any>AuthGuard('jwt'), RolesGuard)
+  @UseGuards(GraphQLJwtAuthGuard, GraphQLRolesGuard)
   async update(@Args() vm: TodoVm): Promise<TodoVm> {
-    const { id, content, level, isCompleted } = vm;
-
-    if (!vm || !id) {
-      throw new HttpException('Missing parameters', HttpStatus.BAD_REQUEST);
-    }
-
-    const exist = await this.todoService.findById(id);
-
-    if (!exist) {
-      throw new HttpException(`${id} Not found`, HttpStatus.NOT_FOUND);
-    }
-
-    if (exist.isCompleted) {
-      throw new HttpException('Already completed', HttpStatus.BAD_REQUEST);
-    }
-
-    exist.content = content;
-    exist.isCompleted = isCompleted;
-    exist.level = level;
-
-    const updated = await this.todoService.update(id, exist);
-    return this.todoService.map<TodoVm>(updated.toJSON());
+    return super.updateTodo(vm);
   }
 
   @Mutation('deleteTodo')
   @Roles(UserRole.Admin, UserRole.User)
-  // @UseGuards(<any>AuthGuard('jwt'), RolesGuard)
+  @UseGuards(GraphQLJwtAuthGuard, GraphQLRolesGuard)
   async delete(@Args('id') id: string): Promise<TodoVm> {
-    const deleted = await this.todoService.delete(id);
-    return this.todoService.map<TodoVm>(deleted.toJSON());
+    return super.deleteTodo(id);
   }
 
   @Subscription('todoCreated')
+  // TODO: guards???
   todoCreated() {
     return {
       subscribe: () => pubSub.asyncIterator('todoCreated'),
--- server/src/todo/todo.service.ts
+++ server/src/todo/todo.service.ts
@@ -9,12 +9,12 @@ import { TodoParams } from './models/view-models/todo-params.model';
 @Injectable()
 export class TodoService extends BaseService<Todo> {
   constructor(
-    @InjectModel(Todo.modelName) private readonly _todoModel: ModelType<Todo>,
-    private readonly _mapperService: MapperService
+    @InjectModel(Todo.modelName) private readonly todoModel: ModelType<Todo>,
+    private readonly mapperService: MapperService
   ) {
     super();
-    this._model = _todoModel;
-    this._mapper = _mapperService.mapper;
+    this._model = todoModel;
+    this._mapper = mapperService.mapper;
   }
 
   async createTodo(params: TodoParams): Promise<Todo> {
