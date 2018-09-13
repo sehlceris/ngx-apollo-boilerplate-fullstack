63dcbe720bd4c62e842eb2f07270fccc0b81ecb4
Christopher Lee <sehlceris@protonmail.com>
2018-09-13 00:17:04 -0700

    move responsibility of client generation to the client

--- client/src/app/feature-modules/static/about/about.component.ts
+++ client/src/app/feature-modules/static/about/about.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ROUTE_ANIMATIONS_ELEMENTS } from 'app/core/index';
 import { UserHttpApiService } from '@app/core/services/api/user-http-api.service';
-import {LoginWithUsernameGQL} from '@app/generated/anms-graphql-client';
-import {BoundLogger, LogService} from '@app/core/services';
+import { LoginWithUsernameGQL } from '@app/generated/anms-graphql-client';
+import { BoundLogger, LogService } from '@app/core/services';
 
 @Component({
   selector: 'anms-about',
@@ -19,18 +19,17 @@ export class AboutComponent implements OnInit {
   constructor(
     public userHttpApiService: UserHttpApiService,
     public loginWithUsernameGQL: LoginWithUsernameGQL,
-    private logService: LogService,
+    private logService: LogService
   ) {}
 
   ngOnInit() {
     console.log('graphql logging in...');
-    this.loginWithUsernameGQL.mutate({
-      username: '000000',
-      password: '000000',
-    })
-      .pipe(
-        this.log.tapObservableForLogging('graphql login')
-      )
+    this.loginWithUsernameGQL
+      .mutate({
+        username: '000000',
+        password: '000000',
+      })
+      .pipe(this.log.tapObservableForLogging('graphql login'))
       .subscribe();
   }
 }
