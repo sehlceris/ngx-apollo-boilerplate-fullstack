2aedd132d5fdc7e076b3fead2f5b474ea862d376
Christopher Lee <sehlceris@protonmail.com>
2018-09-18 00:10:56 -0700

    refinements

--- client/src/app/feature-modules/static/about/about.component.ts
+++ client/src/app/feature-modules/static/about/about.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ROUTE_ANIMATIONS_ELEMENTS } from 'app/core/index';
-import { UserHttpApiService } from '@app/core/services/api/user-http-api.service';
-import {
-  LoginWithUsername,
-  LoginWithUsernameGQL,
-} from '@app/generated/anms-graphql-client';
 import { BoundLogger, LogService } from '@app/core/services';
-import { ExecutionResult, FetchResult } from 'apollo-link';
-import { map } from 'rxjs/operators';
 import { AuthService } from '@app/core/auth/auth.service';
 
 @Component({
@@ -23,13 +16,8 @@ export class AboutComponent implements OnInit {
   private log: BoundLogger = this.logService.bindToNamespace('AboutComponent');
 
   constructor(
-    private authService: AuthService,
     private logService: LogService
   ) {}
 
   ngOnInit() {}
-
-  loginWithGraphQl() {
-    this.authService.loginWithUsername('000000', '000000').subscribe();
-  }
 }
