ef548d653d5f79568de980a979669498087a7271
Chris Lee <sehlceris@protonmail.com>
2018-09-20 12:55:21 -0700

    scss it

--- client/src/app/core/auth/forgot-password/forgot-password.component.ts
+++ client/src/app/core/auth/forgot-password/forgot-password.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'anms-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css'],
+  styleUrls: ['./forgot-password.component.scss'],
 })
 export class ForgotPasswordComponent implements OnInit {
   constructor() {}
