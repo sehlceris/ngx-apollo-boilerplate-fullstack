f2f40f56b4cec93244197dfc9050ce7241e0ddc8
Christopher Lee <sehlceris@protonmail.com>
2018-09-21 01:14:46 -0700

    auth page

--- client/src/app/core/auth/auth-routing.module.ts
+++ client/src/app/core/auth/auth-routing.module.ts
@@ -4,6 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from '@app/core/auth/auth.component';
 import { LoginComponent } from '@app/core/auth/login/login.component';
 import { ForgotPasswordComponent } from '@app/core/auth/forgot-password/forgot-password.component';
+import { RegisterComponent } from '@app/core/auth/register/register.component';
 
 const routes: Routes = [
   {
@@ -23,7 +24,7 @@ const routes: Routes = [
       },
       {
         path: 'register',
-        component: LoginComponent,
+        component: RegisterComponent,
         data: { title: 'anms.auth.register' },
       },
       {
