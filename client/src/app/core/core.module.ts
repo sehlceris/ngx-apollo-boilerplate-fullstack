bc4d7ec5b4a4a72e9fffbe7aa5b0804912096d87
Christopher Lee <sehlceris@protonmail.com>
2018-09-20 23:02:04 -0700

    fix build

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -101,7 +101,9 @@ export class CoreModule {
     parentModule: CoreModule
   ) {
     if (parentModule) {
-      throw new Error('CoreModule is already loaded. Import only in AppModule');
+      throw new Error(
+        'CoreModule is already loaded. Import only in AppModule?'
+      );
     }
   }
 }
