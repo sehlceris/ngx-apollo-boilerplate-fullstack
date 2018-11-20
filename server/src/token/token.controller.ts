16fae3437ddb29fd20436cb6bde6ae088dee4a61
Christopher Lee <sehlceris@protonmail.com>
2018-11-20 00:09:25 -0800

    remove unused imports

--- server/src/token/token.controller.ts
+++ server/src/token/token.controller.ts
@@ -3,10 +3,8 @@ import { ApiBadRequestResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { ApiException } from '../shared/api-exception.model';
 import {JwtSingleUseUserPayload} from '../shared/auth/jwt-payload.model';
 import {HttpTokenGuard} from '../shared/guards/http/http-token-guard';
-import { GetOperationId } from '../shared/utilities/get-operation-id.helper';
 import {UserVm} from '../user/models/view-models/user-vm.model';
 import { UserApiService } from '../user/user-api.service';
-import {Token} from './models/token.model';
 import { TokenApiService } from './token-api.service';
 
 @Controller('token')
